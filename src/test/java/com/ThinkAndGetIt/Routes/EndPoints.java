ee5110a2269df83fb8844fc5d867b0ff21084654
hirwa <hirwagad767@gmail.com>
2026-05-26 11:36:42 +0200

    ft: added reset password test

--- src/test/java/com/ThinkAndGetIt/Routes/EndPoints.java
+++ src/test/java/com/ThinkAndGetIt/Routes/EndPoints.java
@@ -4,4 +4,6 @@ public class EndPoints {
     public static final String Register = "auth/register";
     public static final String Login = "auth/login";
     public static final String Forgot_Password = "auth/forgot-password";
+    public static final String PasswordReset = "auth/reset-password";
+
 }
