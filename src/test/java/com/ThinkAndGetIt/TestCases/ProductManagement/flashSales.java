8897ffc95d617c0dd143969a8dba8d0c45e6f663
hirwa <hirwagad767@gmail.com>
2026-06-09 13:44:26 +0200

    ft:added flash sale product assertion

--- src/test/java/com/ThinkAndGetIt/TestCases/ProductManagement/flashSales.java
+++ src/test/java/com/ThinkAndGetIt/TestCases/ProductManagement/flashSales.java
@@ -5,22 +5,27 @@ import com.ThinkAndGetIt.ReusableMethods.Methods;
 import com.ThinkAndGetIt.Routes.EndPoints;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
+import static org.hamcrest.Matchers.*;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 public class flashSales extends BaseTest {
     @Test
-    public void getTrendingProductsSuccessTest() {
+    public void getFlashSalesProductsSuccessTest() {
         Response response = Methods.GetMethod(EndPoints.Products + "/flash-sales");
-        boolean success = response.path("success");
-        String message = response.path("message");
+
         assertThat(response.statusCode(), equalTo(200));
-        assertThat(success, equalTo(true));
-        assertThat(message, equalTo("Success"));
+        assertThat(response.path("success"), equalTo(true));
+        List<Boolean> flashSaleFlags = response.path("data.isFlashSale");
+        assertThat("Every product returned must have isFlashSale set to true",
+                flashSaleFlags, everyItem(equalTo(true)));
+
+
     }
     @Test
-    public void getTrendingProductsInvalidAPi() {
+    public void getFlashSalesProductsInvalidAPi() {
         Response response = Methods.GetMethod(EndPoints.Products + "/flashin");
 
         boolean success = response.path("success");
