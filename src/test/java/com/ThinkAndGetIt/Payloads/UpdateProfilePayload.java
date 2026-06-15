025b94f21da7e9b7c87f4391bcffc58e89b997b2
hirwa <hirwagad767@gmail.com>
2026-06-15 14:47:25 +0200

    ft:fixed update profile

---
+++ src/test/java/com/ThinkAndGetIt/Payloads/UpdateProfilePayload.java
@@ -0,0 +1,23 @@
+package com.ThinkAndGetIt.Payloads;
+
+public class UpdateProfilePayload {
+    private String firstName;
+    private String lastName;
+    private String phone;
+
+    public UpdateProfilePayload(String firstName, String lastName, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+    }
+
+    // Getters and Setters
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+}
---
+++ src/test/java/com/ThinkAndGetIt/TestCases/UserManagement/UpdateUserProfile.java
@@ -0,0 +1,48 @@
+package com.ThinkAndGetIt.TestCases.UserManagement;
+
+import com.ThinkAndGetIt.Base.BaseTest;
+import com.ThinkAndGetIt.Payloads.UpdateProfilePayload;
+import com.ThinkAndGetIt.ReusableMethods.Methods;
+import com.ThinkAndGetIt.ReusableMethods.TestData;
+import io.restassured.response.Response;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class UpdateUserProfile extends BaseTest {
+
+    private String authToken;
+
+    @BeforeClass
+    public void setUpUserToken() {
+        this.authToken = TestData.token;
+    }
+
+    @Test
+    public void testUpdateUserProfileSuccessfully() {
+        String updatedFirstName = "Automation_" + System.currentTimeMillis();
+        String updatedLastName = "QA";
+        String updatedPhone = "+1234567890";
+
+        UpdateProfilePayload bodyPayload = new UpdateProfilePayload(updatedFirstName, updatedLastName, updatedPhone);
+
+        Response response = Methods.putMethod("/users/profile", bodyPayload, authToken);
+
+        assertThat(response.statusCode(), equalTo(200));
+
+        if (response.path("data") != null) {
+            assertThat(response.path("data.firstName"), equalTo(updatedFirstName));
+            assertThat(response.path("data.lastName"), equalTo(updatedLastName));
+            assertThat(response.path("data.phone"), equalTo(updatedPhone));
+        }
+    }
+
+    @Test
+    public void testUpdateProfileWithoutAuthentication() {
+        UpdateProfilePayload bodyPayload = new UpdateProfilePayload("Jane", "Doe", "+1999999999");
+        Response response = Methods.putMethod("/users/profile", bodyPayload, "");
+        assertThat(response.statusCode(), anyOf(equalTo(401), equalTo(403)));
+    }
+}
